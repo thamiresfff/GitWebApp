a8ed2959c66bda05551144d6fef4a4c55057c673
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2025-06-27 21:13:23 -0300

    Commit Hotfix

--- src/main/java/com/example/WebApp/HomeController.java
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -26,4 +26,9 @@ public class HomeController {
   public @ResponseBody String greetingEclipse() { 
     return "Hello, Eclipse!"; 
   }
+
+  @RequestMapping("/hotfix")
+  public @ResponseBody String greetingHotfix() {
+    return "Hello, Hotfix!";
+  }
 }  
